ce8dad3cc74f9b2915ef4c80cd19a6622e402d50
Diego Trujillo <datrujillog@siesa.com>
2022-09-23 07:37:50 -0500

    Environments

---
+++ app.js
@@ -0,0 +1,34 @@
+require('colors');
+const { port,env } = require('./src/config/environment');
+
+
+
+
+
+
+
+   module.exports = {getGenres} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(); // outputs green text
+console.log("***************".red);
+console.log(env.red);
+console.log("***************".red);
---
+++ src/config/environment.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+module.exports = {
+    port: process.env.PORT,
+    env: process.env.NODE_ENV,
+  
+}
