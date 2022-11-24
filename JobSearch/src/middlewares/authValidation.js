944c789c830bcc5e5566dc296ec201d293fe4a46
datrujillod <diegotrujillo0210@gmail.com>
2022-11-23 23:14:18 -0500

    /* Listo */

--- JobSearch/src/middlewares/authValidation.js
+++ JobSearch/src/middlewares/authValidation.js
@@ -2,7 +2,7 @@
 
 const jwt = require("jsonwebtoken");
 const { SECRET_KEY } = require("../config/environment/environment");
-const { unauthorized } = require("../helpers/sendStatus");
+const { unauthorized,forbidden } = require("../helpers/sendStatus");
 
 
 function authValidation(req,res,next){
@@ -30,7 +30,7 @@ function authValidation(req,res,next){
             }catch({message,name}){
                 // const message = error.message
                 // const name = error.name
-                return res.status(403).json({
+                return res.status(forbidden).json({
                     error:true,
                     message,
                     type:name
@@ -42,7 +42,7 @@ function authValidation(req,res,next){
 
     
 
-    return res.status(403).json({
+    return res.status(forbidden).json({
         error:true,
         message:"Insufficient permissions"
     })
@@ -52,7 +52,7 @@ function adminValidation(req,res,next){
     if(req.user.role==="admin"){
         return next()
     }else{
-        return res.status(403).json({
+        return res.status(forbidden).json({
             error:true,
             message:"Insufficient permissions"
         })
@@ -62,7 +62,7 @@ function applicantValidation(req,res,next){
     if(req.user.role==="applicant"){
         return next()
     }else{
-        return res.status(403).json({
+        return res.status(forbidden).json({
             error:true,
             message:"Insufficient permissions"
         })
@@ -72,7 +72,7 @@ function employerValidation(req,res,next){
     if(req.user.role==="employer"){
         return next()
     }else{
-        return res.status(403).json({
+        return res.status(forbidden).json({
             error:true,
             message:"Insufficient permissions"
         })
@@ -82,7 +82,7 @@ function employerAdminValidation(req,res,next){
     if(req.user.role==="employer" || req.user.role==="admin"){
         return next()
     }else{
-        return res.status(403).json({
+        return res.status(forbidden).json({
             error:true,
             message:"Insufficient permissions"
         })
